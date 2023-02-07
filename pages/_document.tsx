a89822b07a25bf584cc1fd6e3b65e4fed27589d8
jelon12111 <jelon1211@gmail.com>
2023-02-07 21:26:53 +0100

    html static home

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="stylesheet" href="assets/css/main.css" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
